a786584bc1ea74048e7bc6ea58ab3908628ae402
roulanx <70559062+roulanx@users.noreply.github.com>
2026-08-30 15:20:37 +0800

    Galilea V19: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='galilea-v18-shell';
+const CACHE='galilea-v19-shell';
 const SHELL=['/','/index.html','/manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
