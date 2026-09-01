5b765b38624fbc4e319a31eca2c3f5531195cb26
roulanx <70559062+roulanx@users.noreply.github.com>
2026-09-01 14:25:54 +0800

    Refresh cache for worship operations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='galilea-v28-schedule-bulletin-template';
+const CACHE='galilea-v29-worship-operations';
 const SHELL=['/','/index.html','/manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
