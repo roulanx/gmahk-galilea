7e173efcabd474b3b4ee40ff1b1f11694c58f4e8
roulanx <70559062+roulanx@users.noreply.github.com>
2026-09-01 14:43:17 +0800

    Refresh presenter media cache v30

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='galilea-v29-worship-operations';
+const CACHE='galilea-v30-presenter-media';
 const SHELL=['/','/index.html','/manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
