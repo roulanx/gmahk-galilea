bda8e316d47a261040cdd747f769766cf19b5f04
roulanx <70559062+roulanx@users.noreply.github.com>
2026-09-01 11:53:47 +0800

    Refresh cache for premium schedule fullscreen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='galilea-v26-schedule-summary-presenter';
+const CACHE='galilea-v27-premium-schedule-presenter';
 const SHELL=['/','/index.html','/manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
